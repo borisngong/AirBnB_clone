073b968654074b8792fc247665a3fc7433986e68
borisngong <borisngong@gmail>
2024-02-10 10:51:09 -0800

    A class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -13,21 +13,32 @@ class BaseModel:
     numerous models is represented by the BaseModel class.
 
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         A constructor for classes that initializes a new BaseModel instance
+
+        Args:
+            *args: Positional arguments.
+            **kwargs: Keyword arguments.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         Returns the BaseModel instance represented in a string
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                         self.id,
-                                         self.__dict__)
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """
@@ -42,9 +53,8 @@ class BaseModel:
         BaseModel instance
         """
         data_instance_dict = self.__dict__.copy()
-        data_instance_dict['__class__'] = self.__class__.__name__
+        data_instance_dict['__class__'] = type(self).__name__
         data_instance_dict['created_at'] = self.created_at.isoformat()
         data_instance_dict['updated_at'] = self.updated_at.isoformat()
 
         return data_instance_dict
-
