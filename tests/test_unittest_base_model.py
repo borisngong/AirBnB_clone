c8660478c893921bd99e2ce161a77c68b73fb823
borisngong <borisngong@gmail>
2024-02-10 07:11:08 -0800

    A class BaseModel

--- tests/test_unittest_base_model.py
+++ tests/test_unittest_base_model.py
@@ -9,49 +9,58 @@ from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
-    def setUp(self):
+    def test_init(self):
         """
-        Assemble the test fixture. Every test method is called after this one
-        For testing purposes, it creates an instance of the BaseModel class
+        Test the initialization of a BaseModel instance.
         """
-        self.boro_model = BaseModel()
+        my_model = BaseModel()
 
-    def test_if_id_is_string(self):
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.created_at)
+
+    def test_(self):
         """
         Verify whether the BaseModel instance's 'id' field is of type string.
         """
-        self.assertIsInstance(self.boro_model.id, str)
+        my_model = BaseModel()
+
+        self.assertIsInstance(my_model.id, str)
 
     def test_if_created_at_is_datetime(self):
         """
-        Verify whether the BaseModel instance's 'created_at' field is of type
-        datetime
+        Verify whether the BaseModel instance's 'created_at' field is of type datetime.
         """
-        self.assertIsInstance(self.boro_model.created_at, datetime)
+        my_model = BaseModel()
+
+        self.assertIsInstance(my_model.created_at, datetime)
 
     def test_if_updated_at_is_datetime(self):
         """
-        Verify whether the BaseModel instance's 'updated_at' field is of type
-        datetime
+        Verify whether the BaseModel instance's 'updated_at' field is of type datetime.
         """
-        self.assertIsInstance(self.boro_model.updated_at, datetime)
+        my_model = BaseModel()
+
+        self.assertIsInstance(my_model.updated_at, datetime)
 
     def test_save_updates_updated_at(self):
         """
-        Verify whether the BaseModel instance's "updated_at" attribute is
-        updated by the "save" method.
+        Verify whether the BaseModel instance's "updated_at" attribute is updated by the "save" method.
         """
-        former_updated_at = self.boro_model.updated_at
-        self.boro_model.save()
-        self.assertNotEqual(former_updated_at, self.boro_model.updated_at)
-        self.assertGreater(self.boro_model.updated_at, former_updated_at)
+        my_model = BaseModel()
+
+        former_updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(former_updated_at, my_model.updated_at)
+        self.assertGreater(my_model.updated_at, former_updated_at)
 
     def test_to_dict_returns_dict(self):
         """
-        Verify whether the BaseModel instance's dictionary representation is
-        returned by the 'to_dict' method.
+        Verify whether the BaseModel instance's dictionary representation is returned by the 'to_dict' method.
         """
-        boro_dict = self.boro_model.to_dict()
+        my_model = BaseModel()
+
+        boro_dict = my_model.to_dict()
         self.assertIsInstance(boro_dict, dict)
         self.assertIn('id', boro_dict)
         self.assertIn('created_at', boro_dict)
