175ef714f9466558cd5307800e69396140e91ce7
borisngong <borisngong@gmail>
2024-02-11 09:21:05 -0800

    The console CRUD

--- console.py
+++ console.py
@@ -5,6 +5,10 @@ is provided by this module.
 """
 
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import json
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,6 +22,9 @@ class HBNBCommand(cmd.Cmd):
     Represents the string the user sees while using the command-line
     interface
     """
+    __available_classes = {"BaseModel": BaseModel}
+    __storage = FileStorage()
+
     def emptyline(self):
         """
         On an empty line, take no action
@@ -49,6 +56,142 @@ class HBNBCommand(cmd.Cmd):
         Provide helpful information for the 'quit' command.
         """
         print("Quit command to exit the program")
+
+    def do_create(self, user_input):
+        """
+       Establishes a new instance of the user-specified class.
+
+        Usage: create <class_name>
+        """
+        self.__storage.reload()
+        args = shlex.split(user_input)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        target_class = args[0]
+        if target_class not in self.__available_classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = self.__available_classes[target_class]()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, user_input):
+        """
+        shows the instance's string representation that is determined
+        by the instance ID and class name
+
+        Usage: show <class_name> <instance_id>
+        """
+        args = shlex.split(user_input)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        target_class = args[0]
+        if target_class not in self.__available_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = target_class + '.' + obj_id
+        if key not in self.__storage.all():
+            print("** no instance found **")
+            return
+        print(self.__storage.all()[key])
+
+    def do_destroy(self, user_input):
+        """
+        Removes the instance that is indicated by the instance ID
+        and class name.
+
+        Usage: destroy <class_name> <instance_id>
+        """
+        args = shlex.split(user_input)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        target_class = args[0]
+        if target_class not in self.__available_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = target_class + '.' + obj_id
+        if key not in self.__storage.all():
+            print("** no instance found **")
+            return
+        del self.__storage.all()[key]
+        self.__storage.save()
+
+    def do_all(self, user_input):
+        """
+        shows all instances or instances of a particular class string
+        represention
+
+        Usage: all [class_name]
+        """
+        args = shlex.split(user_input)
+        if len(args) == 0:
+            print([str(obj) for obj in self.__storage.all().values()])
+            return
+        target_class = args[0]
+        if target_class not in self.__available_classes:
+            print("** class doesn't exist **")
+            return
+        print([str(obj) for key, obj in self.__storage.all().items()
+               if key.startswith(target_class + '.')])
+
+    def do_update(self, user_input):
+        """
+        Updates an instance's attributes using the class name,
+        instance ID, attribute name, and new attribute value as
+        input.
+
+        Usage: update <class_name> <instance_id> <attribute_name>
+        <attribute_value>
+        """
+        args = shlex.split(user_input)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        target_class = args[0]
+        if target_class not in self.__available_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = target_class + '.' + instance_id
+        if key not in self.__storage.all():
+            print("** no instance found **")
+            return
+        target_instance = self.__storage.all()[key]
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        attr_name = args[2]
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attr_value_str = args[3]
+
+        # Create the attribute if it doesn't exist
+        if not hasattr(target_instance, attr_name):
+            setattr(target_instance, attr_name, "")
+
+        attr_type = type(getattr(target_instance, attr_name))
+        try:
+            attr_value = attr_type(attr_value_str)
+            setattr(target_instance, attr_name, attr_value)
+            self.__storage.save()
+        except Exception:
+            pass
 
 
 if __name__ == "__main__":
