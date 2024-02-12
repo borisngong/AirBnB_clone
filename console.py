10bbe26d2f32955e89085f6158bf1da87b6502f6
borisngong <borisngong@gmail>
2024-02-11 17:26:57 -0800

    The console update

--- console.py
+++ console.py
@@ -1,169 +1,121 @@
-#!/usr/bin/python3
-"""
-An interface for command-line interaction with the application
-is provided by this module.
-"""
-
 import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-import json
 import shlex
 
 
 class HBNBCommand(cmd.Cmd):
     """
-   The program can be interfaced with through the command-line interface
-   provided by the HBNBCommand class
+    Command-line interface for interacting with a file storage system.
     """
 
-    prompt = "(hbnb) "
-    """
-    Represents the string the user sees while using the command-line
-    interface
-    """
-    __available_classes = {"BaseModel": BaseModel}
+    prompt = '(hbnb) '
+    __classes = {"BaseModel": BaseModel}
     __storage = FileStorage()
 
-    def emptyline(self):
+    def do_create(self, line):
         """
-        On an empty line, take no action
-        """
-        pass
-
-    def do_EOF(self, user_input):
-        """
-        Command in charge of safely ending the program when the
-        user types 'EOF' (Ctrl+D).
-        """
-        print("")
-        return True
-
-    def help_EOF(self):
-        """
-        Provide help information for the 'EOF' command.
-        """
-        print("Command to exit the program elegantly 'EOF' (Ctrl+D)")
-
-    def do_quit(self, user_input):
-        """
-        When the user types "quit," the program will exit.
-        """
-        return True
-
-    def help_quit(self):
-        """
-        Provide helpful information for the 'quit' command.
-        """
-        print("Quit command to exit the program")
-
-    def do_create(self, user_input):
-        """
-       Establishes a new instance of the user-specified class.
+        Creates a new instance of a class specified by the user.
 
         Usage: create <class_name>
         """
-        args = shlex.split(user_input)
+        self.__storage.reload()  # Reload storage before creating a new instance
+        args = shlex.split(line)
         if len(args) == 0:
             print("** class name missing **")
             return
-        target_class = args[0]
-        if target_class not in self.__available_classes:
+        class_name = args[0]
+        if class_name not in self.__classes:
             print("** class doesn't exist **")
             return
-        new_instance = self.__available_classes[target_class]()
+        new_instance = self.__classes[class_name]()
         new_instance.save()
         print(new_instance.id)
 
-    def do_show(self, user_input):
+    def do_show(self, line):
         """
-        shows the instance's string representation that is determined
-        by the instance ID and class name
+        Displays the string representation of an instance specified by the class name and instance ID.
 
         Usage: show <class_name> <instance_id>
         """
-        args = shlex.split(user_input)
+        args = shlex.split(line)
         if len(args) == 0:
             print("** class name missing **")
             return
-        target_class = args[0]
-        if target_class not in self.__available_classes:
+        class_name = args[0]
+        if class_name not in self.__classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         obj_id = args[1]
-        key = target_class + '.' + obj_id
+        key = class_name + '.' + obj_id
         if key not in self.__storage.all():
             print("** no instance found **")
             return
         print(self.__storage.all()[key])
 
-    def do_destroy(self, user_input):
+    def do_destroy(self, line):
         """
-        Removes the instance that is indicated by the instance ID
-        and class name.
+        Deletes an instance specified by the class name and instance ID.
 
         Usage: destroy <class_name> <instance_id>
         """
-        args = shlex.split(user_input)
+        args = shlex.split(line)
         if len(args) == 0:
             print("** class name missing **")
             return
-        target_class = args[0]
-        if target_class not in self.__available_classes:
+        class_name = args[0]
+        if class_name not in self.__classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         obj_id = args[1]
-        key = target_class + '.' + obj_id
+        key = class_name + '.' + obj_id
         if key not in self.__storage.all():
             print("** no instance found **")
             return
         del self.__storage.all()[key]
         self.__storage.save()
 
-    def do_all(self, user_input):
+    def do_all(self, line):
         """
-        shows all instances or instances of a particular class string
-        represention
+        Displays the string representation of all instances or instances of a specific class.
 
         Usage: all [class_name]
         """
-        args = shlex.split(user_input)
+        args = shlex.split(line)
         if len(args) == 0:
             print([str(obj) for obj in self.__storage.all().values()])
             return
-        target_class = args[0]
-        if target_class not in self.__available_classes:
+        class_name = args[0]
+        if class_name not in self.__classes:
             print("** class doesn't exist **")
             return
-        print([str(obj) for key, obj in self.__storage.all().items()
-               if key.startswith(target_class + '.')])
+        print([str(obj) for key, obj in self.__storage.all().items() if key.startswith(class_name + '.')])
 
-    def do_update(self, user_input):
+    def do_update(self, line):
         """
-        Responsible for updating an instance's attributes using the class
-        name, instance ID, attribute name, and new attribute value as input.
-        Usage:
-        update <class_name> <instance_id> <attribute_name> <attribute_value>
+        Updates the attributes of an instance specified by the class name, instance ID, attribute name, and new attribute value.
+
+        Usage: update <class_name> <instance_id> <attribute_name> <attribute_value>
         """
-        args = shlex.split(user_input)
+        args = shlex.split(line)
         if len(args) == 0:
             print("** class name missing **")
             return
-        target_class = args[0]
-        if target_class not in self.__available_classes:
+        class_name = args[0]
+        if class_name not in self.__classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         obj_id = args[1]
-        key = target_class + '.' + obj_id
+        key = class_name + '.' + obj_id
         if key not in self.__storage.all():
             print("** no instance found **")
             return
@@ -186,9 +138,40 @@ class HBNBCommand(cmd.Cmd):
             attr_value = attr_type(attr_value_str)
             setattr(obj, attr_name, attr_value)
             self.__storage.save()
-        except Exception:
-            pass
+        except ValueError:
+            print("Attribute value must be of type {}".format(attr_type.__name__))
+            
+    def do_EOF(self, user_input):
+        """
+        Command responsible for exiting the program elegantly when the user enters 'EOF' (Ctrl+D).
+        """
+        print()
+        return True
+    
+    def help_EOF(self):
+        """
+        Provide help information for the 'EOF' command.
+        """
+        print("Command to exit the program elegantly 'EOF' (Ctrl+D)")
 
+    def do_quit(self, user_input):
+        """
+        Exit the program when the user enters 'quit'.
+        """
+        return True
+    
+    def help_quit(self):
+        """
+        Provide help information for the 'quit' command.
+        """
+        print("Quit command to exit the program")
+    
+    def emptyline(self):
+        """
+        Called when an empty line is entered in the prompt.
+        """
+        pass
+        
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,89 +1,42 @@
 #!/usr/bin/python3
-"""
-Module for working with the FileStorage class
-"""
 
 import json
 import os
 from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
-    """
-    An object file storage system is represented by this class.
-    It is possible to serialize, store, and retrieve objects to
-    and from a JSON file.
-    """
     __file_path = "file.json"
     __objects = {}
-    __instance = None
-
-    def __init__(self):
-        """
-        Initializes the FileStorage instance.
-        It loads the data into the __objects dictionary and populates it
-        if the file.json file exists and if it does prints and empty dict
-        """
-        if not FileStorage.__instance:
-            if os.path.exists(self.__file_path):
-                with open(self.__file_path, "r") as file:
-                    serialized_data = json.load(file)
-                    for key, obj_dict in serialized_data.items():
-                        class_name, obj_id = key.split(".")
-                        class_obj = eval(class_name)
-                        obj = class_obj(**obj_dict)
-                        self.__objects[key] = obj
-            else:
-                self.__objects = {}
-            FileStorage.__instance = self
 
     def all(self):
-        """
-        Returns the __objects dictionary, which includes every item that
-        has been stored.
-
-        Returns: dict: An object dictionary with keys that follow the
-        syntax "ClassName.object_id".
-        """
         return self.__objects
 
-    def new(self, in_obj):
-        """
-        introduces a new object into the dictionary of __objects.
-
-        Args: obj (BaseModel): The item that has to be included in
-            the dictionary.
-        """
-        key = f"{in_obj.__class__.__name__}.{in_obj.id}"
-        self.__objects[key] = in_obj
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """
-        Stores the objects in the file.json file after serializing them
-        from the __objects dictionary.
-        """
         serialized_data = {}
-        for key, in_obj in self.__objects.items():
-            serialized_data[key] = in_obj.to_dict()
+        for key, obj in self.__objects.items():
+            serialized_data[key] = obj.to_dict()
+
         with open(self.__file_path, "w") as file:
             json.dump(serialized_data, file)
 
     def reload(self):
-        """
-        loads the contents of the file again.JSON data set.
-        Takes the serialized data from the file, deserializes it,
-        and adds the deserialized objects to the __objects dictionary
-        after clearing the dictionary.
-        """
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                try:
+            try:
+                with open(self.__file_path, "r") as file:
                     serialized_data = json.load(file)
-                    self.__objects = {}
                     for key, obj_dict in serialized_data.items():
                         class_name, obj_id = key.split(".")
-                        class_obj = eval(class_name)
-                        in_obj = class_obj(**obj_dict)
-                        self.__objects[key] = in_obj
-                except Exception:
-                    pass
+                        if class_name == User:
+                            obj = User(**obj_dict)
+                        else:
+                            class_obj = eval(class_name)
+                        obj = class_obj(**obj_dict)
+                        self.__objects[key] = obj
+            except Exception:
+                pass
